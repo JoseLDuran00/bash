0e2d7235aefd33d94521e179c08a0ab1af3e62d0
Jose Duran <joselduran@outlook.com>
2023-10-24 00:28:03 -0300

    commit inicial

---
+++ intro/02-comentarios.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Hola Mordedor ! "
+
+#Todo este texto no será ejecutado
+
+<<comentario
+Hola
+Conejo
+Peludo
+comentario
+
+echo "este texto si se muestra "
+
+#
+# Este es nuestro comentario
+# multi linea
+#
